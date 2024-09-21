af84f3b2b28729dc6e7dd42622dbed2bc0da4cde
Arridha Amrad <arridhaamrad@gmail.com>
2024-09-22 02:56:59 +0700

    suggested users and like posts users ok

--- src/lib/drizzle/queries/fetchPostLikes.ts
+++ src/lib/drizzle/queries/fetchPostLikes.ts
@@ -1,19 +1,9 @@
 import db from "@/lib/drizzle/db";
-import { TInfiniteResult, TUserIsFollow } from "./type";
-import { PostLikesTable } from "../schema";
+import { TInfiniteResult } from "./type";
+import { FollowingsTable, PostLikesTable, UsersTable } from "../schema";
 import { eq, sql } from "drizzle-orm";
 
-const fetchMyFollowingIds = async (authUserId: string) => {
-  if (authUserId === "") return [];
-  const data = await db.query.FollowingsTable.findMany({
-    where(fields, operators) {
-      return operators.eq(fields.userId, authUserId);
-    },
-  });
-
-  const followings = data.map(({ followId }) => followId);
-  return followings;
-};
+const LIMIT = 10;
 
 type Args = {
   postId: string;
@@ -22,60 +12,56 @@ type Args = {
   date?: Date;
 };
 
+const querySumLikes = async (postId: string) => {
+  const [result] = await db
+    .select({
+      sum: sql<number>`
+        CAST(COUNT(${PostLikesTable}) AS Int)
+      `,
+    })
+    .from(PostLikesTable)
+    .where(eq(PostLikesTable.postId, postId));
+  return result.sum;
+};
+
+const queryUsers = async (postId: string, authUserId?: string) => {
+  return db
+    .select({
+      id: UsersTable.id,
+      name: UsersTable.name,
+      avatar: UsersTable.avatar,
+      username: UsersTable.username,
+      isFollow: sql<boolean>`
+      CASE WHEN EXISTS (
+        SELECT 1 FROM ${FollowingsTable}
+        WHERE ${FollowingsTable.userId} = ${authUserId}
+        AND ${FollowingsTable.followId} = ${UsersTable.id}
+      ) THEN true
+        ELSE false
+      END
+    `,
+    })
+    .from(PostLikesTable)
+    .where(eq(PostLikesTable.postId, postId))
+    .innerJoin(UsersTable, eq(PostLikesTable.userId, UsersTable.id))
+    .limit(LIMIT);
+};
+
+export type TLikeUsers = Awaited<ReturnType<typeof queryUsers>>[number];
+
 export const fetchPostLikes = async ({
   postId,
   authUserId,
   page = 1,
   date = new Date(),
-}: Args): Promise<TInfiniteResult<TUserIsFollow>> => {
+}: Args): Promise<TInfiniteResult<TLikeUsers>> => {
   //
-  const getTotalLikes = async () => {
-    const [result] = await db
-      .select({
-        sum: sql<number>`cast(count(${PostLikesTable.postId}) as int)`,
-      })
-      .from(PostLikesTable)
-      .where(eq(PostLikesTable.postId, postId));
-    return result.sum;
-  };
-
-  const getLikes = async () => {
-    const likes = await db.query.PostLikesTable.findMany({
-      columns: {},
-      with: {
-        user: {
-          columns: {
-            id: true,
-            name: true,
-            avatar: true,
-            username: true,
-          },
-        },
-      },
-      where({ postId: pId }, { eq, and }) {
-        return and(eq(pId, postId));
-      },
-    });
-    return likes;
-  };
-
-  const [total, likes, followings] = await Promise.all([
-    getTotalLikes(),
-    getLikes(),
-    fetchMyFollowingIds(authUserId ?? ""),
-  ]);
-
-  const populatedUsers = likes.map(({ user }) => {
-    const usr = {
-      ...user,
-      isFollow: !!followings.find((f) => f === user.id),
-    };
-    return usr;
-  });
+  const total = await querySumLikes(postId);
+  const users = await queryUsers(postId, authUserId);
 
   return {
     date,
-    data: populatedUsers,
+    data: users,
     page,
     total,
   };
--- src/lib/drizzle/queries/fetchSuggestedUsers.ts
+++ src/lib/drizzle/queries/fetchSuggestedUsers.ts
@@ -1,39 +1,58 @@
 import db from "@/lib/drizzle/db";
 import { TSearchUser } from "./type";
+import { FollowingsTable, UsersTable } from "../schema";
+import { sql } from "drizzle-orm";
+
+const query2 = async (userId: string) => {
+  const result = await db.execute(sql`
+    SELECT 
+      ${UsersTable.id},
+      ${UsersTable.username}, 
+      ${UsersTable.avatar}, 
+      ${UsersTable.name}
+    FROM ${UsersTable}
+    WHERE ${UsersTable.id} NOT IN (
+      SELECT ${FollowingsTable.followId} 
+      FROM ${FollowingsTable}
+      WHERE ${FollowingsTable.userId} = ${userId}
+    )
+    AND ${UsersTable.id} != ${userId}
+    `);
+
+  return result.rows;
+};
+
+// const query = async (userId: string) => {
+//   const followingIds = await db
+//     .select({
+//       followId: FollowingsTable.followId,
+//     })
+//     .from(FollowingsTable)
+//     .where(eq(FollowingsTable.userId, userId));
+
+//   const ids = followingIds.map((f) => f.followId);
+
+//   const result = await db
+//     .select({
+//       id: UsersTable.id,
+//       username: UsersTable.username,
+//       avatar: UsersTable.avatar,
+//       name: UsersTable.name,
+//     })
+//     .from(UsersTable)
+//     .where(and(not(inArray(UsersTable.id, ids)), ne(UsersTable.id, userId)));
+
+//   const r2 = await query2(userId);
+//   console.log(r2);
+
+//   return result;
+// };
 
 const fetchSuggestedUsers = async (
   authUserId: string,
 ): Promise<TSearchUser[]> => {
-  const followings = await db.query.FollowingsTable.findMany({
-    columns: {},
-    with: {
-      follow: {
-        columns: {
-          id: true,
-        },
-      },
-    },
-    where: ({ userId }, { eq }) => eq(userId, authUserId),
-  });
-
-  const users = await db.query.UsersTable.findMany({
-    columns: {
-      id: true,
-      avatar: true,
-      username: true,
-      name: true,
-    },
-    where: ({ id }, { notInArray, and, ne }) =>
-      and(
-        notInArray(
-          id,
-          followings.map((f) => f.follow.id),
-        ),
-        ne(id, authUserId),
-      ),
-  });
-
-  return users;
+  const users = await query2(authUserId);
+  return users as TSearchUser[];
 };
 
 export default fetchSuggestedUsers;
