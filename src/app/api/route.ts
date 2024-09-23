dcf8af58b09edb245fee3bd01579a871428492f0
Arridha Amrad <arridhaamrad@gmail.com>
2024-09-23 19:07:16 +0700

    okk

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,15 @@
 import db from "@/lib/drizzle/db";
-import { PostsTable, SearchUsersTable, UsersTable } from "@/lib/drizzle/schema";
-import { desc, eq, lte, sql } from "drizzle-orm";
+import {
+  FollowingsTable,
+  PostLikesTable,
+  PostsTable,
+  SearchUsersTable,
+  UserInfoTable,
+  UsersTable,
+} from "@/lib/drizzle/schema";
+import { aliasedTable, desc, eq, lte, sql } from "drizzle-orm";
 import { NextResponse } from "next/server";
+import crypto from "crypto";
 
 // const query = db
 //   .select()
@@ -11,24 +19,23 @@ import { NextResponse } from "next/server";
 //   .limit(5)
 //   .prepare("query_feed_posts");
 
-const query = async (userId: string) => {
-  return db
-    .select({
-      id: UsersTable.id,
-      username: UsersTable.username,
-      avatar: UsersTable.avatar,
-      name: UsersTable.name,
-    })
-    .from(SearchUsersTable)
-    .where(eq(SearchUsersTable.userId, userId))
-    .innerJoin(UsersTable, eq(UsersTable.id, SearchUsersTable.searchId));
-};
+// const query = async (userId: string) => {
+//   return db
+//     .select({
+//       id: UsersTable.id,
+//       username: UsersTable.username,
+//       avatar: UsersTable.avatar,
+//       name: UsersTable.name,
+//     })
+//     .from(SearchUsersTable)
+//     .where(eq(SearchUsersTable.userId, userId))
+//     .innerJoin(UsersTable, eq(UsersTable.id, SearchUsersTable.searchId));
+// };
 
 export const GET = async (req: Request) => {
-  const date = new Date("2024-06-29T06:31:21.144Z");
-  const userId = "4f70c468-ea07-4396-a6c0-609948f48e69";
-
-  const users = await query(userId);
-
-  return NextResponse.json(users);
+  // const date = new Date("2024-06-29T06:31:21.144Z");
+  // const userId = "4f70c468-ea07-4396-a6c0-609948f48e69";
+  // let uuid = crypto.randomUUID();
+  // const users = await query(userId, uuid);
+  // return NextResponse.json(users);
 };
--- src/lib/drizzle/queries/fetchUserProfile.ts
+++ src/lib/drizzle/queries/fetchUserProfile.ts
@@ -1,54 +1,74 @@
 import db from "@/lib/drizzle/db";
-import { PostsTable } from "@/lib/drizzle/schema";
-import { eq, sql } from "drizzle-orm";
+import {
+  FollowingsTable,
+  PostsTable,
+  UserInfoTable,
+  UsersTable,
+} from "@/lib/drizzle/schema";
+import { aliasedTable, eq, sql } from "drizzle-orm";
 import { unstable_cache } from "next/cache";
+import crypto from "crypto";
 
 type Args = {
   username: string;
   authUserId?: string;
 };
 
+const followers = aliasedTable(FollowingsTable, "followers");
+const followings = aliasedTable(FollowingsTable, "followings");
+const query = async (userId: string, authUserId?: string) => {
+  return db
+    .select({
+      id: UsersTable.id,
+      username: UsersTable.username,
+      name: UsersTable.name,
+      avatar: UsersTable.avatar,
+      bio: UserInfoTable.bio,
+      gender: UserInfoTable.gender,
+      occupation: UserInfoTable.occupation,
+      website: UserInfoTable.website,
+      sumPosts: sql<number>`
+        CAST(COUNT(DISTINCT ${PostsTable.id}) AS Int)
+      `,
+      sumFollowers: sql<number>`
+        CAST(COUNT(DISTINCT ${followers}) AS Int)
+      `,
+      sumFollowings: sql<number>`
+        CAST(COUNT(DISTINCT${followings}) AS Int)
+      `,
+      isFollowed: sql<boolean>`
+        CASE WHEN EXISTS(
+          SELECT 1
+          FROM ${FollowingsTable}
+          WHERE ${FollowingsTable.followId} = ${UsersTable.id}
+          AND ${FollowingsTable.userId} = ${authUserId}
+        )
+          THEN true
+          ELSE false
+        END
+      `,
+    })
+    .from(UsersTable)
+    .where(eq(UsersTable.id, userId))
+    .innerJoin(UserInfoTable, eq(UserInfoTable.userId, UsersTable.id))
+    .leftJoin(followings, eq(followings.userId, UsersTable.id))
+    .leftJoin(PostsTable, eq(PostsTable.userId, UsersTable.id))
+    .leftJoin(followers, eq(followers.followId, UsersTable.id))
+    .groupBy(UsersTable.id, UserInfoTable.id);
+};
+
+export type TProfile = Awaited<ReturnType<typeof query>>[number];
+
 const getProfile = async ({ username, authUserId }: Args) => {
-  const user = await db.query.UsersTable.findFirst({
-    with: {
-      followers: true,
-      followings: true,
-      userInfo: true,
-    },
-    columns: {
-      id: true,
-      avatar: true,
-      name: true,
-      username: true,
-    },
-    where(fields, operators) {
-      return operators.eq(fields.username, username);
-    },
-  })
-
-    .then(async (user) => {
-      if (user) {
-        const [{ sumPosts }] = await db
-          .select({
-            sumPosts: sql<number>`count(${PostsTable.id})`,
-          })
-          .from(PostsTable)
-          .where(eq(PostsTable.userId, user.id));
-        return {
-          ...user,
-          followers: user.followers.length,
-          followings: user.followings.length,
-          isFollowed: !authUserId
-            ? false
-            : !!user.followers.find((f) => f.userId === authUserId),
-          sumPosts,
-        };
-      } else {
-        return null;
-      }
-    });
-
-  return user;
+  const [user] = await db
+    .select({ id: UsersTable.id })
+    .from(UsersTable)
+    .where(eq(UsersTable.username, username));
+  if (!authUserId) {
+    authUserId = crypto.randomUUID();
+  }
+  const data = await query(user.id, authUserId);
+  return data;
 };
 
 export const fetchUserProfile = unstable_cache(
